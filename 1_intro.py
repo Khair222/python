ec7b23691603a59249378dcb0cc602445bc2bf79
Khair222 <fathulkhair393gmail.com>
2024-09-03 12:35:18 +0700

    perkuliahan 1

---
+++ 1_intro.py
@@ -0,0 +1,21 @@
+# deklarasi vaiabel
+a = float(10.5)
+b = 5
+C = a * b
+
+# operasi (Operation) -> tamabah (+), kurang(-), bagi(/), kali(*)
+print(a)
+print(b)
+print(C)
+
+perkalian = a * b
+pembagian = a / b
+pengurangan = a - b 
+modulo = a % b
+pangkat = a ** b
+
+print(perkalian)
+print(pembagian)
+print(pengurangan)
+print(modulo)
+print(pangkat)
